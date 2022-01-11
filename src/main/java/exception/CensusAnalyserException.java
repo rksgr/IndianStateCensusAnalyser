702e37cebb0336d25ad9125956eb215fe2769211
rksgr <r_kjha@yahoo.com>
2022-01-11 13:39:05 +0530

    Added: Class to handle Custom Exceptions thrown from file CensusAnalyser

--- src/main/java/exception/CensusAnalyserException.java
+++ src/main/java/exception/CensusAnalyserException.java
@@ -1,2 +1,20 @@
-package exception;public class CensusAnalyserException {
+package exception;
+
+public class CensusAnalyserException extends Exception{
+    enum Exception_Type{
+        CENSUS_FILE_PROBLEM,
+        UNABLE_TO_PARSE;
+    }
+    Exception_Type type ;
+
+    public CensusAnalyserException(String message, Exception_Type type){
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, Exception_Type type, Throwable cause){
+        super(message,cause);
+        this.type = type;
+    }
+
 }
