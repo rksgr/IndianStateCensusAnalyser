974347583c0d3188ef4979260c5a68cdb6692c2a
rksgr <r_kjha@yahoo.com>
2022-01-11 13:41:13 +0530

    Added: Test Cases to check entries in csv file

--- src/test/java/CensusAnalyserTest.java
+++ src/test/java/CensusAnalyserTest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class CensusAnalyserTest {
+import exception.CensusAnalyserException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import service.CensusAnalyser;
+
+public class CensusAnalyserTest {
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords(){
+        try{
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndianCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assertions.assertEquals(2,numOfRecords);
+        }catch(CensusAnalyserException e){
+
+        }
+    }
 }
