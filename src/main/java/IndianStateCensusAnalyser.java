18370f07d162bdb66db9f1208bafbdbd351865a4
rksgr <r_kjha@yahoo.com>
2022-01-11 12:12:59 +0530

    Added Introductory message

---
+++ src/main/java/IndianStateCensusAnalyser.java
@@ -0,0 +1,6 @@
+public class IndianStateCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to India!");
+        System.out.println("Today we shall deal with problems related to Indian State Census Analysis");
+    }
+}
